30686c57ea98b98596e1b67c2df342c0ec4fb511
jixuecao <2386900869@qq.com>
2022-05-16 16:39:28 +0800

    Update main.py

--- Real-world-Anomaly-Detection-in-Surveillance-Videos-pytorch/main.py
+++ Real-world-Anomaly-Detection-in-Surveillance-Videos-pytorch/main.py
@@ -31,12 +31,12 @@ def train(epoch):
     correct = 0
     total = 0
     for batch_idx, (normal_inputs, anomaly_inputs) in enumerate(zip(normal_train_loader, anomaly_train_loader)):
-        #  print('normal_inputs shape:', normal_inputs.shape) # torch.Size([30, 32, 2048]) 
+        #  print('normal_inputs shape:', normal_inputs.shape) # torch.Size([30, 32, 2048])
         inputs = torch.cat([anomaly_inputs, normal_inputs], dim=1)
-        #  print('inputs shape:', inputs.shape) # torch.Size([30, 64, 2048])  
+        #  print('inputs shape:', inputs.shape) # torch.Size([30, 64, 2048])
         batch_size = inputs.shape[0]
         inputs = inputs.view(-1, inputs.size(-1)).to(device)
-        outputs = model(inputs)  #  outputs.shape: torch.Size([1920, 1]) 
+        outputs = model(inputs)  # torch.Size([1920, 1]) 
         loss = criterion(outputs, batch_size)
         optimizer.zero_grad()
         loss.backward()
@@ -52,15 +52,16 @@ def test_abnormal(epoch):
         for i, (data, data2) in enumerate(zip(anomaly_test_loader, normal_test_loader)):
             inputs, gts, frames = data
             inputs = inputs.view(-1, inputs.size(-1)).to(torch.device('cuda'))
-            score = model(inputs)
+            score = model(inputs) # score.shape: torch.Size([32, 1]) 
             score = score.cpu().detach().numpy()
-            score_list = np.zeros(frames[0])
-            step = np.round(np.linspace(0, frames[0]//16, 33))
+            score_list = np.zeros(frames[0]) # (716) 每个视频所拥有的帧数
+            step = np.round(np.linspace(0, torch.div(frames, 16, rounding_mode='floor'), 33))  # [ 0.  1.  3.  4.,,,, 38. 40. 41. 43. 44.] 共33个元素 
 
             for j in range(32):
-                score_list[int(step[j])*16:(int(step[j+1]))*16] = score[j]
+                score_list[int(step[j])*16:(int(step[j+1]))*16] = score[j]   # 最后的几帧没有赋值,得分为0
 
             gt_list = np.zeros(frames[0])
+            #  print(gts)  #video1: [tensor([240]), tensor([750])] video2: [tensor([240]), tensor([390]), tensor([540]), tensor([1800])]
             for k in range(len(gts)//2):
                 s = gts[k*2]
                 e = min(gts[k*2+1], frames)
@@ -71,7 +72,7 @@ def test_abnormal(epoch):
             score2 = model(inputs2)
             score2 = score2.cpu().detach().numpy()
             score_list2 = np.zeros(frames2[0])
-            step2 = np.round(np.linspace(0, frames2[0]//16, 33))
+            step2 = np.round(np.linspace(0, torch.div(frames, 16, rounding_mode='floor'), 33))
             for kk in range(32):
                 score_list2[int(step2[kk])*16:(int(step2[kk+1]))*16] = score2[kk]
             gt_list2 = np.zeros(frames2[0])
